7c7c6d334749573e757451b678477b6aa06694aa
Saransh <saransh.jain@metadologie.com>
2026-01-12 15:02:23 +0530

    Push for proxy api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://43.205.174.122/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
